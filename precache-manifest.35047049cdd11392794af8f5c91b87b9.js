affee78826362bb0311ab2d2bad4d5ee04b99dc2
aleksandrovfe <aleksandrov7296@gmail.com>
2020-06-20 07:38:05 +0300

    Updates

---
+++ precache-manifest.35047049cdd11392794af8f5c91b87b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385bb7c69cba8e4010d611c65ff5f1f9",
+    "url": "/two-points/index.html"
+  },
+  {
+    "revision": "8a0343e4e0ab2ed1ec81",
+    "url": "/two-points/static/css/main.cb563117.chunk.css"
+  },
+  {
+    "revision": "ba653f549180d49b05ef",
+    "url": "/two-points/static/js/2.45ccdb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/two-points/static/js/2.45ccdb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0343e4e0ab2ed1ec81",
+    "url": "/two-points/static/js/main.53386c60.chunk.js"
+  },
+  {
+    "revision": "1d5433577f28c6918279",
+    "url": "/two-points/static/js/runtime-main.1db910fe.js"
+  }
+]);
